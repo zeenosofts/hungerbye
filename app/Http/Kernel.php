1392ca92f37d630a8a34da8a633cbc19427844d7
Muhammad Zohaib <zohaibazhar359@gmail.com>
2020-08-19 04:22:22 -0700

    Middleware setup

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+           // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
@@ -61,7 +61,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'api' => \App\Http\Middleware\APIAuthenticate::class
+        'api' => \App\Http\Middleware\APIAuthenticate::class,
+        'csrf' => \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 
     /**
